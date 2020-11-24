978c63b75a06aa7a16e45d5a0b879a66bf021b17
Ivan <delfatti.ivan@gmail.com>
2020-11-24 15:22:43 +0100

    Add dynamic filter

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@
 
     data: {
         cds: [],
+        options: [ 'All', ],
+        selected: 'All',
     },
 
     created(){
@@ -15,16 +17,38 @@
         .then( valid => {
             // handle success
             this.cds = valid.data.response
-            console.log(this.cds);
+
+            this.cds.forEach( el  => {
+                if ( ! this.options.includes( el.genre ) ) {
+                    this.options.push(el.genre)
+                }
+            });
         })
         .catch( invalid => {
             // handle error
             console.log(invalid);
-        })
+        });
 
     },
 
     methods: {
+
+        genreSelected(){
+            axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then( valid => {
+            // handle success
+            this.cds = valid.data.response
+
+            if ( this.selected !== 'all' ) {
+                this.cds = this.cds.filter( el => el.genre === this.selected);
+            };
+
+        })
+        .catch( invalid => {
+            // handle error
+            console.log(invalid);
+        });
+        },
 
     },
  });
